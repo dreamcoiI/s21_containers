8ca3300dd922bb698185a24204d95c86f0413e67
casimira <casimira@student.21-school.ru>
2023-05-26 20:51:27 +0300

    update s21_containers

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,9 +1,9 @@
-#ifndef 
-#define 
+#ifndef S21_CONTAINERS_H
+#define S21_CONTAINERS_H
 
 #include "s21_list.h"
 #include "s21_containers/s21_vector.h"
 #include "s21_containers/s21_map.h"
 #include "s21_containers/s21_set.h"
 #include "s21_containers/s21_vector.h"
-#endif
+#endif //S21_CONTAINERS_H
