c420fd344e06ed37541c59d6badde1be5f9dbc72
casimira <casimira@student.21-school.ru>
2023-05-31 20:45:50 +0300

    remove old version list

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -5,4 +5,6 @@
 #include "s21_containers/s21_map.h"
 #include "s21_containers/s21_set.h"
 #include "s21_containers/s21_vector.h"
+#include "s21_containers/s21_queue.h"
+#include "s21_containers/s21_stack.h"
 #endif  // S21_CONTAINERS_H
